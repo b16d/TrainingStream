519c76e115bcfbc01e54871aec45782dd5ff3acb
b16d <albanclevy@yahoo.fr>
2020-11-02 14:45:27 +0100

    Add subject 2

---
+++ src/test/java/Sujet2.java
@@ -0,0 +1,89 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class Sujet2 {
+	/**
+	 * Given a list of words, create an output list that contains
+	 * only the odd-length words, converted to upper case.
+	 */
+	@Test 
+	public void d1_upcaseOddLengthWords() {
+		List<String> input = Arrays.asList(
+				"alfa", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+		List<String> result = input.stream()
+				.filter(p -> p.length() %2 != 0)
+				.map(c -> c.toUpperCase())
+				.collect(Collectors.toList());
+		
+		result.forEach(System.out::println);
+		assertEquals(Arrays.asList("BRAVO", "CHARLIE", "DELTA", "FOXTROT"), result);
+	}
+
+	/**
+	 * Take the third through fifth words of the list, extract the
+	 * second letter from each, and join them, separated by commas,
+	 * into a single string. Watch for off-by-one errors.
+	 */
+	@Test
+	public void d2_joinStreamRange() {
+		List<String> input = Arrays.asList(
+				"alfa", "bravo", "charlie", "delta", "echo", "foxtrot");
+
+		String result = input.stream()
+		.skip(2)
+		.limit(3)
+		.map(p -> "" + p.charAt(1)).collect(Collectors.joining(","));
+
+		assertEquals("h,e,c", result);
+	}
+
+	/**
+	 * Select the longest words from the input list. That is, select the words
+	 * whose lengths are equal to the maximum word length.
+	 */
+	@Test
+	public void d6_selectLongestWords() {
+		List<String> input = Arrays.asList(
+				"alfa", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel");
+
+		//Cas 1
+//		Predicate<String> pre 
+		OptionalInt test  = input.stream().mapToInt(e -> e.length()).max();
+		
+//		System.out.println(test);
+		List<String> result = input.stream()
+				.filter(p -> p.length() == test.getAsInt())
+				.collect(Collectors.toList()); // TODO
+
+		//Cas 2
+		 List<String> map = input.stream()
+		.collect(Collectors.groupingBy(String::length, TreeMap::new, Collectors.toList())).lastEntry().getValue();	
+
+	
+		assertEquals(Arrays.asList("charlie", "foxtrot"), map);
+	}
+
+
+	/**
+	 * Select the list of words from the input list whose length is greater than
+	 * the word's position in the list (starting from zero) .
+	 */
+	@Test @Ignore
+	public void d7_selectByLengthAndPosition() {
+		List<String> input = Arrays.asList(
+				"alfa", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel");
+
+		List<String> result = null; // TODO
+
+		assertEquals(Arrays.asList("alfa", "bravo", "charlie", "delta", "foxtrot"), result);
+	}
+}
